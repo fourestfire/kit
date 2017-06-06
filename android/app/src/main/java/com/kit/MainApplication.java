fccdcec6c4b1c0e6cb33ede7e23c03c49801e9ed
fourestfire <kewliez@gmail.com>
2017-06-05 23:24:26 -0400

    add: support for realm

--- android/app/src/main/java/com/kit/MainApplication.java
+++ android/app/src/main/java/com/kit/MainApplication.java
@@ -3,6 +3,7 @@ package com.kit;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.realm.react.RealmReactPackage;
 import com.cmcewen.blurview.BlurViewPackage;
 import com.wix.interactable.Interactable;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RealmReactPackage(),
             new BlurViewPackage(),
             new Interactable()
       );
